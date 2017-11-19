9bf004ef85fc28452abfbe182caa7a27b71cef94
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2017-11-19 14:58:23 +0200

    sent first response from server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 var http = require('http');
-var server = http.createServer();
+
+
+var message = "I am happy to be coding along";
+
+function handler(request, response) {
+	response.writeHead(200, {'ContentType':'text/html'});
+	response.write(message);
+	response.end();
+}
+
+
+var server = http.createServer(handler);
+
 
 server.listen(3000, function () {
 	console.log("Server listening at port 3000, ready to accept requests");
