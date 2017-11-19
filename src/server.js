9a9506d299c30d5df6db33860c5423a912dfb090
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2017-11-19 14:41:05 +0200

    Craeted server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var server = http.createServer();
+
+server.listen(3000, function () {
+	console.log("Server listening at port 3000, ready to accept requests");
+}
+	)
+
+
+
